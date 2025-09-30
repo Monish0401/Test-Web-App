b321016ebb5d06530d24c4e3fc28b5e233ccaa42
Monish C <86337147+Monish0401@users.noreply.github.com>
2025-09-30 14:53:42 +0530

    classic responsive nav bar

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function openNav() {
       document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("menuBtn").style.display = "none";
     }
 
     function closeNav() {
       document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("menuBtn").style.display = "inline-block";
     }
