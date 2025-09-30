31f7db355ed96bd9dced317e40af53c3ac2f4cd6
Monish C <86337147+Monish0401@users.noreply.github.com>
2025-09-30 14:47:10 +0530

    CHANGE IN STYLE AND SCRIPT

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("menuBtn").style.display = "none";
+    }
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("menuBtn").style.display = "inline-block";
+    }
