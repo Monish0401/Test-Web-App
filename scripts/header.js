b40a14a13e0f516f6b37dbdb337441c8c815747d
Monish0401 <Monish0401@users.noreply.github.com>
2025-10-08 05:41:39 +0000

    deploy: 7dec4cff1b4206c1f47785f26d0fd5743df0020a

---
+++ scripts/header.js
@@ -0,0 +1,9 @@
+function header(){
+    return(
+        <div className="header">
+        <img src="static/logo/Space_Applications_Centre_logo.png" alt="SAC Logo" />
+        <h1>Software Defined Radio(SDR) Payload Commanding User Inteface</h1>
+        <img src="static/logo/Indian_Space_Research_Organisation_Logo.png" alt="ISRO Logo" />
+      </div>
+    );
+}
